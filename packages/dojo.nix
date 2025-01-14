aace3a19ef038a09d7d1c611ae237f73ef511d49
Valentin Ricard <me@valentin.red>
2025-01-14 14:21:09 +0100

    feat: Added patches for scarb

--- packages/dojo.nix
+++ packages/dojo.nix
@@ -12,9 +12,16 @@
 
   mkDojo = {
     version,
+    cairoVersion,
     srcHash,
     depsHash,
+    cairoHash,
   }: let
+    cairo-zip = pkgs.fetchurl {
+      url = "https://github.com/starkware-libs/cairo/archive/refs/tags/v${cairoVersion}.zip";
+      hash = cairoHash;
+    };
+
     src = pkgs.fetchFromGitHub {
       name = "${name}-${version}-src";
       owner = "dojoengine";
@@ -23,11 +30,39 @@
       hash = srcHash;
     };
 
-    cargoDeps = rustPlatform.fetchCargoVendor {
+    unpatchedCargoDeps = rustPlatform.fetchCargoVendor {
       inherit src;
       name = "${name}-${version}-deps";
       hash = depsHash;
     };
+
+    cargoDeps = let
+      patchMetadata = pkgs.substituteAll {
+        src = ../patches/scarb/scarb-metadata.patch;
+        cairoZip = "${cairo-zip}";
+      };
+      patchScarb = pkgs.substituteAll {
+        src = ../patches/scarb/scarb.patch;
+        cairoZip = "${cairo-zip}";
+      };
+    in
+      pkgs.stdenv.mkDerivation {
+        src = unpatchedCargoDeps;
+
+        name = "${name}-${version}-deps-patch";
+        phases = "unpackPhase patchPhase installPhase";
+
+        patchPhase = ''
+          BUILD_METADATA_DIR=$(echo ./*/scarb-build-metadata-*)
+          BUILD_SCARB_DIR=$(echo ./*/scarb-[0-9]*)
+          echo "Applying patch for scarb-build-metadata"
+          ${pkgs.patch}/bin/patch --directory $BUILD_METADATA_DIR -p1 < ${patchMetadata}
+          echo "Applying patch for scarb"
+          ${pkgs.patch}/bin/patch --directory $BUILD_SCARB_DIR -p1 < ${patchScarb}
+        '';
+
+        installPhase = "cp -R ./ $out";
+      };
 
     buildCrate = name:
       rustPlatform.buildRustPackage {
@@ -39,12 +74,10 @@
           openssl
 
           rustPlatform.bindgenHook
+          libclang
 
           protobuf
         ];
-
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-
         name = name;
 
         cargoBuildOptions = ["-p" name];
