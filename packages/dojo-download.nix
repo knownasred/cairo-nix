7b57b5a939a3be149e87cc1b7440336eedf0eb16
Valentin Ricard <me@valentin.red>
2025-07-13 21:27:22 +0200

    fix: Fixed paths for dojo

--- packages/dojo-download.nix
+++ packages/dojo-download.nix
@@ -15,87 +15,24 @@
     name = "dojo-artifacts";
     src = sozoTargz;
     phases = ["unpackPhase"];
+
+    nativeBuildInputs = with pkgs; [autoPatchelfHook makeWrapper];
+    buildInputs = with pkgs; [stdenv.cc.cc];
+
     unpackPhase = ''
-      mkdir -p $out
-      tar -xzf $src -C $out
+      mkdir -p $out/bin
+      tar -xzf $src -C $out/bin
+
+      for file in $out/bin/*; do
+        if [ -f "$file" ] && [ -x "$file" ]; then
+          wrapProgram "$file" \
+            --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
+            pkgs.glibc
+          ]}"
+        fi
+      done
     '';
   };
 in {
   dojo = artifacts;
-
-  dojo-language-server = pkgs.stdenv.mkDerivation {
-    name = "dojo-language-server";
-    src = artifacts;
-    phases = ["unpackPhase" "installPhase"];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp dojo-language-server $out/bin
-    '';
-  };
-
-  katana = pkgs.stdenv.mkDerivation rec {
-    name = "katana";
-    src = artifacts;
-
-    nativeBuildInputs = with pkgs; [autoPatchelfHook makeWrapper];
-    buildInputs = with pkgs; [stdenv.cc.cc];
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin
-      cp ${name} $out/bin
-
-      wrapProgram $out/bin/${name} \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
-        pkgs.glibc
-      ]}"
-
-      runHook postInstall
-    '';
-  };
-
-  sozo = pkgs.stdenv.mkDerivation rec {
-    name = "sozo";
-    src = artifacts;
-
-    nativeBuildInputs = with pkgs; [autoPatchelfHook makeWrapper];
-    buildInputs = with pkgs; [stdenv.cc.cc];
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin
-      cp ${name} $out/bin
-
-      wrapProgram $out/bin/${name} \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
-        pkgs.glibc
-      ]}"
-
-      runHook postInstall
-    '';
-  };
-
-  torii = pkgs.stdenv.mkDerivation rec {
-    name = "torii";
-    src = artifacts;
-
-    nativeBuildInputs = with pkgs; [autoPatchelfHook makeWrapper];
-    buildInputs = with pkgs; [stdenv.cc.cc];
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin
-      cp ${name} $out/bin
-
-      wrapProgram $out/bin/${name} \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
-        pkgs.glibc
-      ]}"
-
-      runHook postInstall
-    '';
-  };
 }
